9cfe8743529aaca62832a72e9591ec02a246cd5c
Wxrlz <164504560+Wxrlz@users.noreply.github.com>
2024-03-23 01:17:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ client.on('ready', async () => {
     .setName('W')
     .setDetails(`•${formatTime()}• 『 𝙅𝘽_𝘾𝟮 』`)
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/919170276569272340/1220394727892062209/d20453532d07f55a667d8a3dbbf1bfd7.gif?ex=660ec828&is=65fc5328&hm=122e682ac439b7361cc7c3f02ab217d2fd413bf01be8becae4546c1647795fc7&') //You can put links in tenor or discord and etc.
+    .setAssetsLargeImage('https://media.discordapp.net/attachments/919170276569272340/1220798226551148704/91a5926602d6261eedfe60798a98b096.gif?ex=66103ff2&is=65fdcaf2&hm=463facfca89c738e4c6b8f21530909e568a803276d3a11dc1343623215a1acf5&=') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('『 𝗝𝗕𝗜𝗦𝗖𝗢𝗠𝗜𝗡𝗚 』') //Text when you hover the Large image
     .setAssetsSmallImage('https://cdn.discordapp.com/attachments/919170276569272340/1220394728453963967/dont_have_bg.gif?ex=660ec829&is=65fc5329&hm=ff1a73289d9ba72c237eaf4e848e9aaba9ba10c19f557d8a8e3d1e4b4c8d94e4&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('P') //Text when you hover the Small image
