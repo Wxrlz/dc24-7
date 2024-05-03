6e2aeb69f6d6b8598d8d1be5cf886b7dce5ae538
Wxrlz <164504560+Wxrlz@users.noreply.github.com>
2024-05-03 22:41:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ client.on('ready', async () => {
     .setName('W')
     .setDetails(`•${formatTime()}• 『 𝙅𝘽_𝙘𝙢𝙧 』`)
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://media.discordapp.net/attachments/919170276569272340/1235264843536142426/bdd09e06930b9fd2bac94b71428cab0f.gif?ex=6633bd88&is=66326c08&hm=ef4e13881acfed6b98286fb473984b001980139c386fda327d581ad83ec94148&=') //You can put links in tenor or discord and etc.
+    .setAssetsLargeImage('https://media.discordapp.net/attachments/919170276569272340/1232669029139746856/e54afabd75adb33464e85f2687b43f87.gif?ex=663580be&is=66342f3e&hm=3d405d732dff8697b19854496fa1b86479e36b1ec659b98709341b000e7dccd7&=') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('『 𝗝𝗕𝗜𝗦𝗖𝗢𝗠𝗜𝗡𝗚 』') //Text when you hover the Large image
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/919170276569272340/1235264844010360994/bdd3360a05b0c02cc9cc766a73bc75fc.gif?ex=6633bd88&is=66326c08&hm=57973da8d375cb9de67823df37bc0ed4c2153e0364b5548d68812df067d5ab0a&=') //You can put links in tenor or discord and etc.
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/919170276569272340/1232669029676748810/Hand.gif?ex=663580be&is=66342f3e&hm=e636fb9ee60cb23522f27941ae5c783821bc3ab585fb9494dae45fdf21b484eb&=') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('Ma money on the floor if u want') //Text when you hover the Small image
-    .addButton('˚ INSTARGRAM 🕷 ˚ ', 'https://www.instagram.com/jxnla_bxnz.xyz/')
+    .addButton('˚ INSTARGRAM 🕷 ˚ ', 'https://www.instagram.com/jxnla_bxnz/')
     .addButton('˚ SOUNDCLOUD ✰ ˚', 'https://soundcloud.com/wxnz-653259588');
 
   client.user.setActivity(r);
